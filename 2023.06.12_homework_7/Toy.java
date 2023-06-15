ce5a1d726cf301f9575d4cc25bfa5c0ca2a7f13f
idkfa <1@idkfa.ru>
2023-06-16 00:09:29 +0500

    Homework 7

---
+++ 2023.06.12_homework_7/Toy.java
@@ -0,0 +1,26 @@
+public class Toy {
+    int id;
+    int frequency;
+    String name;
+
+    public int getID(){
+        return id;
+    }
+    public int getFrequency(){
+        return frequency;
+    }
+
+    public String getName(){
+        return name;
+        
+    }
+
+ 
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return name;
+    }
+
+
+}
